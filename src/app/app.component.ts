28c4febbe9b8e7aa708f9269d4c1e3fcae3f4c22
Giulia <g.bonicolini@key2.it>
2024-09-20 18:27:28 +0200

    input e output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from '../library/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DEMO_angular18';
+  titoloPadre = signal<string>("Signal titolo")
+
+  getOutput(e:boolean){
+    console.log(e);
+    
+  }
 }
+
+
+
--- src/library/card/card.component.ts
+++ src/library/card/card.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
+  //INPUT
+  titoloFiglio = input.required<string>();
 
+  //OUTPUT
+  onNameChange = output<boolean>(); // OutputEmitterRef<string>
+  setNewName() {
+    this.onNameChange.emit(true);
+  }
 }
