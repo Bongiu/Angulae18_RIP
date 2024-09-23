2f100d521f316e23829240820864d3fb963f45b0
Giulia <g.bonicolini@key2.it>
2024-09-23 12:42:56 +0200

    componenti form template e reactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from '../library/card/card.component';
+import { ReactiveFormComponent } from '../library/reactive-form/reactive-form.component';
+import { TemplateFormComponent } from '../library/template-form/template-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, ReactiveFormComponent, TemplateFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
